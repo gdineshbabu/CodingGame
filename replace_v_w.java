84a275f397ade25903f376bd9276104eb419064a
gdineshbabu <118524830+gdineshbabu@users.noreply.github.com>
2023-05-06 10:43:22 +0530

    replace_v_w.java
    Input
    Expected output
    bovvvlet
    bowlet
    02 Test 2
    Input
    Expected output
    bovvvvvlet
    bowlet
    03 Test 3
    Input
    Expected output
    bovvvvwwlet
    bowwwlet
    04 Test 4
    Input
    Expected output
    bolvvvv
    bolw

---
+++ replace_v_w.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+class Solution {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        String M= in.nextLine();
+        String a=M.replaceAll("vvvv", "w");
+        System.out.print(a);
+    }
+}
