97f2659ae3562c0b15433cc42a522cb500760222
gdineshbabu <118524830+gdineshbabu@users.noreply.github.com>
2023-05-08 10:42:38 +0530

    String_ntimes.java
    Input
    Expected output
    asdfa afdas
    asdfaasdfaasdfaasdfaasdfa
    02 Test 2
    Input
    Expected output
    qwqqwq qwq
    qwqqwqqwqqwqqwqqwq
    03 Test 3
    Input
    Expected output
    j lkajsdflkjdslkg
    jjjjjjjjjjjjjjj
    04 Test 4
    Input
    Expected output
    input output
    inputinputinputinputinputinput
    05 Test 5
    Input
    Expected output
    123oiuO*#@ **U*qq9R
    123oiuO*#@123oiuO*#@123oiuO*#@123oiuO*#@123oiuO*#@123oiuO*#@123oiuO*#@123oiuO*#@

---
+++ String_ntimes.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        String a = in.next();
+        String b = in.next();
+        for(int i=0;i<b.length();i++)
+        {
+            System.out.print(a);
+        }
+    }
+}
