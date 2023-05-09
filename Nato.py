2eb72eba06cf1b0590e147ca83924e25e18be979
gdineshbabu <118524830+gdineshbabu@users.noreply.github.com>
2023-05-09 11:00:10 +0530

    Create Nato.py
    To avoid confusion when verbally spelling a name, people sometimes use the word in the NATO alphabet associated with each letter in the name.
    
    Help these people speak the spelling of their names.
    
    ~~~~~~
    
    The NATO alphabet is Alpha, Bravo, Charlie, Delta, Echo, Foxtrot, Golf, Hotel, India, Juliett, Kilo, Lima, Mike, November, Oscar, Papa, Quebec, Romeo, Sierra, Tango, Uniform, Victor, Whiskey, X-ray, Yankee, Zulu
    Input
    Line 1: A string name
    Output
    Line 1: That name spelled out using the NATO alphabet
    Constraints
    name consists only of letters
    Example
    Input
    Lisa
    Output
    Lima India Sierra Alpha

---
+++ Nato.py
@@ -0,0 +1,14 @@
+nato_alphabet = {
+    "A": "Alpha", "B": "Bravo", "C": "Charlie", "D": "Delta", "E": "Echo",
+    "F": "Foxtrot", "G": "Golf", "H": "Hotel", "I": "India", "J": "Juliett",
+    "K": "Kilo", "L": "Lima", "M": "Mike", "N": "November", "O": "Oscar",
+    "P": "Papa", "Q": "Quebec", "R": "Romeo", "S": "Sierra", "T": "Tango",
+    "U": "Uniform", "V": "Victor", "W": "Whiskey", "X": "X-ray", "Y": "Yankee",
+    "Z": "Zulu"
+}
+
+name = input().upper()
+
+spelled_out = [nato_alphabet[char] for char in name if char in nato_alphabet]
+
+print(" ".join(spelled_out))
