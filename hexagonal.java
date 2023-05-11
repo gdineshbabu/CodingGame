3fe66341ee68ed85d5c98341d8c2eacbf1587b52
gdineshbabu <118524830+gdineshbabu@users.noreply.github.com>
2023-05-11 13:24:22 +0530

    hexagonal.java
    Parse the regular hexagon, and tell how much regular unit triangle do you find, if the length of one side of the regular hexagon is n unit(s).
    
    As it is well known, every regular hexagon, that has a positive integer value for the length of one side can be split into a positive integer value of regular triangles, where the regular triangle has sides with 1 unit of length.
    
    For example, if the hexagon has sides with 1 unit of length, it will be split into 6 triangles.
    If the hexagon has sides with 2 units of length, it will be split into 24 triangles.
    Input
    Line 1: The length of one side of the hexagon is n unit
    Output
    Line 1 : The number of regular 1-unit triangles inside the regular hexagon
    Constraints
    0 < n < 7500
    Example
    Input
    1
    Output
    6

---
+++ hexagonal.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        // Write an answer using System.out.println()
+        // To debug: System.err.println("Debug messages...");
+
+        System.out.println(6*(n*n));
+    }
+}
